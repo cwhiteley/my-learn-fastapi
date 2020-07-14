fa4cf362d877003f442a8bb11aaa3ef17a793412
Mark Vartanyan <kolypto@gmail.com>
2020-07-15 00:06:58 +0300

    Docs & examples

--- simplest.py
+++ simplest.py
@@ -2,13 +2,14 @@ from __future__ import annotations
 from datetime import datetime
 from enum import Enum
 from typing import Optional, TypeVar, Generic, List
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, HttpUrl
 from fastapi import FastAPI
 from fastapi import Query, Path, Body
 
 app = FastAPI(
-    # App title, used in docs
-    title='simplest'
+    # App title and version, used in docs
+    title='simplest',
+    version='0.0.1',
 )
 
 # uvicorn simplest:app --reload
@@ -79,14 +80,57 @@ def read_item_by_type(path: str):
 
 # Interface: data model
 class Item(BaseModel):
-    name: str
+    # Pydantic validation
+    # (note: Query(), Path(), Body() are subclasses of Pydantic.field)
+    name: str = Field(..., title='Name', description='Name', min_length=1)
     price: float
     is_offer: Optional[bool] = None
     when: Optional[datetime]
+    image: Optional[Image]  # a forward reference!
 
+    # Customize example
+    class Config:
+        schema_extra = {
+            # Example: will go into the docs
+            "example": {
+                "name": "Foo",
+                "description": "A very nice Item",
+                "price": 35.4,
+                "tax": 3.2,
+            }
+        }
+
+class Image(BaseModel):
+    url: HttpUrl = Field(...,
+                         # Docs info
+                         title='Image URL',
+                         example='http://example.com/example.png')
+    name: str
+
+Item.update_forward_refs()  # when forward references fail to update
 
 # Saving: body as JSON (`item`)
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
 
+
+
+
+
+# Example recursive models
+
+class User(BaseModel):
+    id: int
+    devices: List[Device]
+
+class Device(BaseModel):
+    uid: Optional[int]
+    user: Optional[User]
+
+User.update_forward_refs()
+Device.update_forward_refs()
+
+@app.put('/save')
+def save(user: User):
+    pass
